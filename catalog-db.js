f898fe22dc5f85ba5a14f70835cbb51b78347d5c
Jan Miksovsky <jan@miksovsky.com>
2011-10-24 14:26:05 -0700

    Start rendering links to new catalog pages

---
+++ catalog-db.js
@@ -0,0 +1,30 @@
+/*
+ * The set of controls in the QuickUI Catalog.
+ * Keep this list sorted by control name.
+ */
+var controlRecords = [
+    {
+        name: "CalendarDay",
+        summary: ""
+    },
+    {
+        name: "CalendarMonth",
+        summary: ""
+    },
+    {
+        name: "CalendarMonthWithHeadings",
+        summary: ""
+    },
+    {
+        name: "CalendarWeek",
+        summary: ""
+    },
+    {
+        name: "DaysOfWeek",
+        summary: ""
+    },
+    {
+        name: "MonthName",
+        summary: ""
+    },
+];
