d9efc5538a744be0a46cb4fcf2d13f33bd54719b
Jan Miksovsky <jan@quickui.org>
2013-01-23 11:12:42 -0800

    Restore SampleDialog demo. Import mixins where needed.

--- docs/Dialog/demo.js
+++ docs/Dialog/demo.js
@@ -1,10 +1,33 @@
 function demoDialog() {
 
+var SampleDialog = Dialog.sub({
+    className: "SampleDialog",
+    inherited: {
+        content: [
+            "<h1>Sample Dialog</h1>",
+            { control: LoremIpsum },
+            {
+                control: SampleSpriteButton,
+                ref: "buttonClose",
+                content: "Close",
+                css: { "margin-top": "1em" }
+            }
+        ],
+        width: "600px"
+    },
+    initialize: function() {
+        var self = this;
+        this.$buttonClose().click(function() {
+            self.close();
+        });
+    }
+})
+        
 $demo.append(
     Link.create( "Click for a sample dialog" )
         .click( function() {
-        Dialog.showDialog( SampleDialog );
-    })
+            Dialog.showDialog( SampleDialog );
+        })
 );
 
 }
