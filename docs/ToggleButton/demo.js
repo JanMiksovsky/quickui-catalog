a1cb1879fefdd120926995256900f48df38c733f
Jan Miksovsky <jan@quickui.org>
2013-01-23 13:48:07 -0800

    Improve ToggleButton selected style. Tweak ToggleButton demo.

--- docs/ToggleButton/demo.js
+++ docs/ToggleButton/demo.js
@@ -2,11 +2,13 @@ function demoToggleButton() {
 
 var $buttonBold = ToggleButton.create({
     content: "B",
-    css: { "font-weight": "bold" }
+    css: { "font-weight": "bold" },
+    selected: true
 });
 var $buttonItalic = ToggleButton.create({
     content: "I",
-    css: { "font-style": "italic" }
+    css: { "font-style": "italic" },
+    width: "1.8em"
 });
 var $buttonUnderline = ToggleButton.create({
     content: "U",
@@ -26,11 +28,17 @@ $demo.append(
 );
 
 $demo.on( "click", ".ToggleButton", function() {
+    updateSample();
+});
+
+function updateSample() {
     $sample.css({
         "font-weight": $buttonBold.selected() ? "bold" : "normal",
         "font-style": $buttonItalic.selected() ? "italic" : "normal",
         "text-decoration": $buttonUnderline.selected() ? "underline" : "none"
-    })
-});
+    });
+}
+
+updateSample();
 
 }
